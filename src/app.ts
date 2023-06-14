405ff469fa82b6703f75b021891c16a449ded9d1
Byron Torres <byleto@gmail.com>
2023-06-14 15:17:41 +0200

    docker setup and other configurations

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
-import express, { Application, Request, Response, NextFunction } from "express";
-import { router as userRoutes } from "./routes/user.routes";
-import cors from "cors";
+import express, { Application, Request, Response, NextFunction } from 'express';
+import { router as userRoutes } from './routes/user.routes';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app: Application = express();
+app.use(morgan('dev'));
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: '*' }));
 
-app.use("/", userRoutes);
-
-app.use("/", (req: Request, res: Response, next: NextFunction): void => {
-  res.json({ message: "Hola!." });
-});
+app.use('/', userRoutes);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import app from "./app";
+import app from './app';
 
-const PORT: Number = 5050;
+const PORT: Number = Number(process.env.NODE_DOCKER_PORT || 8081);
 
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
